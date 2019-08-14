ce8a3959080b6fbed3297de6c3e3d18566d9a084
Ann-Sofi Lantz <Anso@Anso.local>
2019-08-14 13:40:09 +0200

    Reading from txt file

--- __tests__/rover-test.js
+++ __tests__/rover-test.js
@@ -1,4 +1,5 @@
 const Rover = require('../rover');
+const fs = require('fs');
 
 test('Turn left', () => {
   const myRover = new Rover(0, 0, 'N');
@@ -32,13 +33,11 @@ test('Process instructions for one rover', () => {
 });
 
 test('Process instructions for many rovers', () => {
-
   const instructions = ['5 5', '1 2 N', 'LMLMLMLMM', '3 3 E', 'MMRMMRMRRM'];
   const expectedOutputs = ['1 3 N', '5 1 E'];
   let roverIndex = 0;
 
   for (let i = 1; i < instructions.length; i = i + 2) {
-
     const startPosition = instructions[i].split(' ');
     const startX = parseInt(startPosition[0]);
     const startY = parseInt(startPosition[1]);
@@ -58,10 +57,38 @@ test('Process instructions for many rovers', () => {
       }
     });
 
-
     expect(myRover.getPosition()).toBe(expectedOutputs[roverIndex]);
     roverIndex++;
-
   }
+});
 
+test('Process instructions for rovers reading outputs from file', () => {
+  const instructions = fs.readFileSync('./__tests__/process-instructions.txt', 'utf-8').split('\n');
+  console.log("instructions from file: ", instructions)
+  
+  const expectedOutputs = ['1 3 N', '5 1 E'];
+  let roverIndex = 0;
+
+  for (let i = 1; i < instructions.length; i = i + 2) {
+    const startPosition = instructions[i].split(' ');
+    const startX = parseInt(startPosition[0]);
+    const startY = parseInt(startPosition[1]);
+    const startDirection = startPosition[2];
+
+    const myRover = new Rover(startX, startY, startDirection);
+    const movements = instructions[i + 1].split('');
+
+    //console.log(movements)
+    movements.forEach(move => {
+      if (move === "M") {
+        myRover.moveForward();
+      } else if (move === "L") {
+        myRover.turnLeft();
+      } else if (move === "R") {
+        myRover.turnRight();
+      }
+    });
+    expect(myRover.getPosition()).toBe(expectedOutputs[roverIndex]);
+    roverIndex++;
+  }
 });
--- main.js
+++ main.js
@@ -7,13 +7,13 @@ let plateau = new Grid();
 let rover1 = new Rover(0, 0, 'N');
 
 
-rover1.turnRight();
-rover1.moveForward();
-rover1.turnLeft();
-rover1.moveForward();
-rover1.moveForward();
-rover1.turnRight();
-rover1.moveForward();
-rover1.moveForward();
+// rover1.turnRight();
+// rover1.moveForward();
+// rover1.turnLeft();
+// rover1.moveForward();
+// rover1.moveForward();
+// rover1.turnRight();
+// rover1.moveForward();
+// rover1.moveForward();
 
 
