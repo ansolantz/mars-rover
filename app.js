b681f38b99ea5e86fbe5d7ae17356e6cb124d613
Ann-Sofi Lantz <Anso@Anso.local>
2019-08-15 17:53:48 +0200

    Multiple instructions in one line

--- app.js
+++ app.js
@@ -9,16 +9,33 @@ var standard_input = process.stdin;
 // Set input character encoding.
 standard_input.setEncoding('utf-8');
 
+moveRover = (rover, movements) => {
+
+    movements.forEach(move => {
+        if (move === "M") {
+            rover.moveForward();
+        } else if (move === "L") {
+            rover.turnLeft();
+        } else if (move === "R") {
+            rover.turnRight();
+        } else if (move.includes('\r\n') || move.includes('\n') || move.includes('\r')) {
+            //Linebreak
+        } 
+        else {
+            console.log(move.replace(/(\r\n|\n|\r)/gm, "") + ' is not a valid command!');
+        }
+    });
+}
 
 processFileInstructions = (instructions) => {
-    console.log('Reading instructions from the file:')
-
     const plateauSize = instructions[0].split(' ');
     const xSize = parseInt(plateauSize[0]);
     const ySize = parseInt(plateauSize[1]);
- 
+
     const plateauGrid = new Grid(xSize, ySize);
     console.log('Plateau size: ' + xSize + ' x ' + ySize)
+
+    //console.log('Moving instructions from file:')
 
     let roverIndex = 1;
     for (let i = 1; i < instructions.length; i = i + 2) {
@@ -30,37 +47,36 @@ processFileInstructions = (instructions) => {
         const myRover = new Rover(startX, startY, startDirection);
         const movements = instructions[i + 1].split('');
 
+       
         console.log('\nMoving rover ' + roverIndex);
-        //console.log(movements)
-        movements.forEach(move => {
-            if (move === "M") {
-                myRover.moveForward();
-            } else if (move === "L") {
-                myRover.turnLeft();
-            } else if (move === "R") {
-                myRover.turnRight();
-            }
-        });
+        console.log('Start position: ' + startX + ' ' + startY + ' ' + startDirection);
+        console.log('Instructions: ' + movements + '\n');
         
+        //console.log(movements)
+        moveRover(myRover, movements);
+
         console.log('------------------------');
         roverIndex++;
-        
+
     }
 }
 
 // Prompt user to input data in console.
-console.log('*******************************************************');
-console.log('Please input text in command line to move the rover.');
+console.log('*********************************************************************************\n');
+console.log('*** MARS ROVER ***')
+console.log('Please input text in command line to move the rover.\n');
+
 console.log('The valid commands are: ‘L’, ‘R’ and ‘M’.');
-console.log('‘L’ and ‘R’ makes the rover spin 90 degrees left or right respectively, without moving from its current spot.');
+console.log('‘L’ and ‘R’ makes the rover spin 90 degrees left or right respectively, \nwithout moving from its current spot.');
 console.log('‘M’ moves the rover forward one grid point.\n');
 
 console.log('You can also run commands from a file. The file name is ’process-instructions.txt’');
-console.log('The program will move rover(s) from the instructions in the file’');
-console.log('To load the file, please type ‘load’ in command line.\n')
+console.log('The program will move rover(s) from the instructions in the file.');
+console.log('These rovers are diferente from the rovers you controle manually.');
+console.log('To load the file, please type ‘file’ in command line.\n')
 
-console.log('To exit, type ‘exit’.')
-console.log('******************************************************');
+console.log('To exit, type ‘exit’.\n')
+console.log('********************************************************************************');
 
 
 const plateau = new Grid(5, 5);
@@ -68,7 +84,8 @@ const rover1 = new Rover(0, 0, 'N');
 
 
 // When user input data and click enter key.
-standard_input.on('data', function (data) {
+// Source: https://www.dev2qa.com/node-js-get-user-input-from-command-line-prompt-example/
+standard_input.on('data', (data) => {
 
     // User input exit.
     if (data === 'exit\n') {
@@ -76,25 +93,16 @@ standard_input.on('data', function (data) {
         console.log('User input complete, program exit.');
         process.exit();
     }
-    else if (data === 'load\n') {
+    else if (data === 'file\n') {
         // Loading instructions from file.
         const instructions = fs.readFileSync('./__tests__/process-instructions.txt', 'utf-8').split('\n');
-        console.log("instructions from file: ", instructions)
+        console.log("Instructions from file: ", instructions)
         processFileInstructions(instructions);
 
-    }
-    else if (data[0] === 'L') {
-        rover1.turnLeft();
-    }
-    else if (data[0] === 'R') {
-        rover1.turnRight();
-    }
-    else if (data[0] === 'M') {
-        rover1.moveForward();
-    }
-    else {
-        // Print user input in console.
-        console.log(data.replace(/(\r\n|\n|\r)/gm, "") + ' is not a valid command!');
+    } else {
+        //Spliting instructions to an array
+        const instructionsArray = data.split('');
+        moveRover(rover1, instructionsArray);
     }
 });
 
